a63f18612e7cbea78ea3eeb090bfa7a79e5cd268
chaospredictor <kuzi81@gmail.com>
2017-03-22 23:08:27 +0200

    working show table

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,58 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/Main.css');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+var DATA = [{name:"pi"}, {name:"cheaps"}, {name:"fdgd"}]
 
-class AppComponent extends React.Component {
+class ProductRow extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+      <tr>
+        <td>{this.props.recipe.name}</td>
+      </tr>
+    );
+  }
+}
+
+class RecipesTable extends React.Component {
+  constructor(props) {
+    super(props);
+  //  this.handleInStockInputChange = this.handleInStockInputChange.bind(this);
+  }
+
+  render() {
+    var rows = [];
+    var lastCategory = null;
+    console.log(this.props.inStockOnly)
+    this.props.recipes.forEach((recipe) => {
+      rows.push(<ProductRow recipe={recipe} key={recipe.id} />);
+    });
+    return (
+      <table>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+}
+
+
+class MainComponent extends React.Component {
+	
+  constructor(props) {
+    super(props);
+		this.state = {
+			recipes: DATA
+		};
+  }
+
+	render() {
+    return (
+      <div className="main">
+				<RecipesTable recipes={this.state.recipes} />,	
       </div>
     );
   }
 }
 
-AppComponent.defaultProps = {
-};
-
-export default AppComponent;
+export default MainComponent;

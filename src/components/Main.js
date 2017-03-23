0a013f21450f2be47031a531946cb3d28d6a3115
chaospredictor <kuzi81@gmail.com>
2017-03-23 23:10:28 +0200

    react-modal added & working open/close dialog window

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,8 +2,22 @@ require('normalize.css/normalize.css');
 require('styles/Main.css');
 
 import React from 'react';
+import Modal from 'react-modal';
 
-var DATA = [{name:"pi"}, {name:"cheaps"}, {name:"fdgd"}]
+var DATA = [{name:"pi", id:0}, {name:"cheaps", id:1}, {name:"fdgd", id:2}]
+
+const appElement = document.getElementById('your-app-element');
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
 
 class ProductRow extends React.Component {
   render() {
@@ -42,22 +56,55 @@ class MainComponent extends React.Component {
   constructor(props) {
     super(props);
 		this.state = {
-			recipes: DATA
+			recipes: DATA,
+      modalIsOpen: false
 		};
 		this.myFunction = this.myFunction.bind(this);
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
   }
 
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.refs.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+	
 	myFunction() {
 		var temp = this.state.recipes.slice()
-		temp.push({name:"newOne"})
-		this.setState({ recipes: temp })
+		temp.push({name:"newOne",id:4})
+		this.setState({ recipes: temp, modalIsOpen: false })
 	}
 
 	render() {
     return (
       <div className="main">
 				<RecipesTable recipes={this.state.recipes} />
-				<button onClick={this.myFunction}>Activate Lasers</button>
+        <button onClick={this.myFunction}>Add</button>
+        <button onClick={this.openModal}>Open Modal</button>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+
+          <h2 ref="subtitle">Hello</h2>
+          <input />
+          <form>
+						<button onClick={this.closeModal}>Close</button>
+            <button onClick={this.myFunction}>Add</button>
+          </form>
+        </Modal>
       </div>
     );
   }

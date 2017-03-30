a73c88f4373487950b25773ab05d28658bfb95ca
chaospredictor <kuzi81@gmail.com>
2017-03-31 01:02:34 +0300

    replace class to className in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,8 @@ class FooterComponent extends React.Component {
   render() {
     return (
 			<footer>
-				<Col md={6}>coded by <a class="text-danger" target="_blank" href="https://www.freecodecamp.com/chaospredictor">Chaos Predictor</a></Col>
-				<Col md={6} className="rightFooter">souces in <a class="text-danger" target="_blank" href="https://github.com/ChaosPredictor/w12-CamperLeaderboard">GitHub</a></Col>
+				<Col md={6}>coded by <a className="text-danger" target="_blank" href="https://www.freecodecamp.com/chaospredictor">Chaos Predictor</a></Col>
+				<Col md={6} className="rightFooter">souces in <a className="text-danger" target="_blank" href="https://github.com/ChaosPredictor/w12-CamperLeaderboard">GitHub</a></Col>
 			</footer>
 		);
   }
